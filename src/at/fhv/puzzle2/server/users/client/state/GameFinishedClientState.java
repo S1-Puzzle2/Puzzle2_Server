fc5703e26565860c580a19d618feb799287b6092
Andreas Sinz <andreas.sinz@aon.at>
2015-01-24 20:10:27 +0100

    Added GameFinished ClientState

---
+++ src/at/fhv/puzzle2/server/users/client/state/GameFinishedClientState.java
@@ -0,0 +1,32 @@
+package at.fhv.puzzle2.server.users.client.state;
+
+import at.fhv.puzzle2.communication.application.command.Command;
+import at.fhv.puzzle2.communication.application.command.commands.GameFinishedCommand;
+import at.fhv.puzzle2.server.SendQueue;
+import at.fhv.puzzle2.server.users.client.Client;
+
+public class GameFinishedClientState extends ClientState {
+    private boolean _isWinning;
+
+    public GameFinishedClientState(Client client) {
+        super(client);
+    }
+
+    public void setIsWinning(boolean isWinning) {
+        _isWinning = isWinning;
+    }
+
+    @Override
+    public ClientState handleCommand(Command command) {
+        return null;
+    }
+
+    @Override
+    public void enter() {
+        GameFinishedCommand gameFinishedCommand = new GameFinishedCommand(_client.getClientID());
+        gameFinishedCommand.setConnection(_client.getConnection());
+        gameFinishedCommand.setIsWinning(_isWinning);
+
+        SendQueue.getInstance().addCommandToSend(gameFinishedCommand);
+    }
+}
